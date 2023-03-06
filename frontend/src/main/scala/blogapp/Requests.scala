176c547dbce7e9b1a268d6d6e5ba1dfe6bc13cfb
Kyle Dinh <kyledinh@gmail.com>
2023-03-06 08:41:28 -0800

    getAllScrawls request for Board Page to list scrawls from db

---
+++ frontend/src/main/scala/blogapp/Requests.scala
@@ -0,0 +1,72 @@
+package blogapp
+
+import blogapp.models.*
+import com.raquo.laminar.api.L.{Owner => _, _}
+import org.scalajs.dom.document
+// import blogapp.models.api.{CreateOwner, CreatePet, CreateVisit, UpdateOwner, UpdatePet, UpdateVisit}
+import sttp.capabilities
+import sttp.client3.*
+import zio.json.*
+import zio.json.internal.RetractReader
+
+import scala.concurrent.Future
+
+object Requests {
+  private val backend: SttpBackend[Future, capabilities.WebSockets] = FetchBackend()
+
+  private val baseUrl =
+    if (document.location.host.contains("localhost"))
+      uri"http://localhost:8080"
+    else
+      uri"http://localhost:8080"
+
+  def getRequest[A: JsonCodec](path: Any*): EventStream[A] = {
+    val request = quickRequest.get(uri"$baseUrl/$path")
+    EventStream.fromFuture(backend.send(request)).map { response =>
+      response.body.fromJson[A] match {
+        case Right(b) => b
+        case Left(e)  => throw new Error(s"Error parsing JSON: $e")
+      }
+    }
+  }
+
+  def deleteRequest(path: Any*): EventStream[Unit] = {
+    val request = quickRequest.delete(uri"$baseUrl/$path")
+    EventStream.fromFuture(backend.send(request)).map(_ => ())
+  }
+
+  def postRequest[In: JsonEncoder, Out: JsonDecoder](body: In)(path: Any*): EventStream[Out] = {
+    val request = quickRequest.post(uri"$baseUrl/$path").body(body.toJson)
+    EventStream.fromFuture(backend.send(request)).map { response =>
+      response.body.fromJson[Out] match {
+        case Right(b) => b
+        case Left(e)  => throw new Error(s"Error parsing JSON: $e")
+      }
+    }
+  }
+
+  def patchRequest[In: JsonEncoder, Out: JsonDecoder](body: In)(path: Any*): EventStream[Out] = {
+    val request = quickRequest.patch(uri"$baseUrl/$path").body(body.toJson)
+    EventStream.fromFuture(backend.send(request)).map { response =>
+      response.body.fromJson[Out] match {
+        case Right(b) => b
+        case Left(e)  => throw new Error(s"Error parsing JSON: $e")
+      }
+    }
+  }
+
+  def getAllScrawls(): EventStream[List[Scrawl]] =
+    getRequest[List[Scrawl]]("scrawls")
+
+  def getScrawls(personId: Uuid): EventStream[List[Scrawl]] =
+    getRequest[List[Scrawl]]("persons", personId.id, "scrawls")
+
+  // def addScrawl(createScrawl: CreateScrawl): EventStream[Scrawl] =
+  //   postRequest[CreateScawl, Scrawl](createScrawl)("scrawls")
+
+
+  implicit lazy val unitDecoder: JsonDecoder[Unit] =
+    new JsonDecoder[Unit] {
+      override def unsafeDecode(trace: List[JsonError], in: RetractReader): Unit = ()
+    }
+}
--- frontend/src/main/scala/blogapp/views/BoardView.scala
+++ frontend/src/main/scala/blogapp/views/BoardView.scala
@@ -1,58 +1,119 @@
 package blogapp.views
 
+import animus.*
 import com.raquo.laminar.api.L.{*, given}
 import blogapp.Component
+import blogapp.models.*
 import blogapp.views.components.{Components}
+import blogapp.{Requests}
 import java.time.LocalDate 
 
+final case class EditableScrawlView(scrawl: Scrawl, reload: () => Unit) extends Component {
+  val isEditingVar = Var(false)
+
+  val body: Div = 
+    div(cls("col-lg-4 mb-4 mb-lg-0"),
+        // data-aos="fade-up" data-aos-delay="0",
+      div(cls("service grayscale"),
+        div(cls("service-img"),
+          img(src("medio/images/img_1.jpg"))
+        ),
+        div(cls("servicce-inner"),
+          h3(s"${scrawl.title}"),
+          p(s"${scrawl.body}")
+        )
+      )
+    // ).amend(
+    //   data-aos("fade-up"),
+    //   data-aos-delay("0")
+    ) 
+
+}
+
 final case class BoardView() extends Component {
-  val body: Div =
-    div(
-      cls("text-l text-justify m-5"),
-      div(
-        cls("mb-2 text-xl text-orange-700 font-bold"),
+
+  // Bus and Signals/Streams
+  val reloadScrawlBus: EventBus[Unit] =
+    new EventBus[Unit] 
+
+  val $scrawls: Signal[List[Scrawl]] =
+    reloadScrawlBus.events.flatMap { _ =>
+        Requests.getAllScrawls()
+      }
+      .toSignal(List.empty)
+
+  reloadScrawlBus.emit(())
+
+  // Elements
+
+  val body: Div = div(
+    reloadScrawlBus.events --> { _ => () },
+    onMountCallback(_ => reloadScrawlBus.emit(())),      
+    div(cls("text-l text-justify m-5"),
+      div(cls("mb-2 text-xl text-orange-700 font-bold"),
         "Scrawl Board"
       ),
-      div(
-        cls("mb-2 text-xl text-gray-400 font-bold"),
+      div(cls("mb-2 text-xl text-gray-400 font-bold"),
         Components.formatDate(LocalDate.now())
       ),
-      div(
-        cls("mb-4"),
-        "A fullstack, idiomatic web app that serves as an example for best utilizing ZIO and the libraries within its ecosystem."
+      div(cls("section bg-light"),
+        div(cls("container"),
+          div(cls("row"),
+            children <-- $scrawls.map { scrawls =>
+              scrawls.map(EditableScrawlView(_, () => reloadScrawlBus.emit(())))
+            }
+          )
+        )
       ),
-      div(
-        cls("mb-4"),
-        "Navigate to the Owners tab to preview a list of preloaded pet owners. Select an owner to do things like view their personal information, add pets, update visits for a pet, and create owners."
-      ),
-      div(
-        cls("mb-4"),
+      div(cls("mb-4"),
         "This version of the website uses Scala 3!"
       ),
-      div(
-        cls("mb-2 text-xl text-gray-400 font-bold"),
-        "Resources"
-      ),
+      div(cls("mb-2 text-xl text-gray-400 font-bold"), "Resources"),
       div(
         bodyLink(
           "GitHub repo for this project",
           "https://github.com/kyledinh/zio-blogapp"
         )
       ),
-      div(
-        cls("mb-4"),
+      div(cls("mb-4"),
         bodyLink(
           "ZIO documentation",
           "https://zio.dev/"
         )
       )
     )
+  )
 
   private def bodyLink(name: String, url: String) =
-    a(
-      cls("text-orange-700 hover:text-orange-600 text-l cursor-pointer"),
+    a(cls("text-orange-700 hover:text-orange-600 text-l cursor-pointer"),
       target("_blank"),
       name,
       href(url)
     )
 }
+
+
+/*
+	<div class="section bg-light">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-lg-4 mb-4 mb-lg-0" data-aos="fade-up" data-aos-delay="0">
+					<div class="service grayscale">
+						<div class="service-img">
+							<a href="#"><img src="medio/images/img_1.jpg" alt="Image" class="img-fluid"></a>
+						</div>
+						<div class="service-inner">
+							<span></span>
+							<h3>Collaborate</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+							<p class="mb-0"><a href="#" class="more">Learn more</a></p>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+  */
